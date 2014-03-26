3a18d0ba755b770c0502a05a37001ed9468f2921
Tuure Laurinolli <tuure.laurinolli@fmi.fi>
2014-03-26 17:36:45 +0200

    Made current time group(s) accessible

--- src/addins/lib/OpenLayers/Layer/Animation/LayerGroupCoordinator.js
+++ src/addins/lib/OpenLayers/Layer/Animation/LayerGroupCoordinator.js
@@ -4,6 +4,8 @@ OpenLayers.Layer.Animation.LayerGroupCoordinator = OpenLayers.Class({
 
     initialize : function(layers, constraints, availableRanges) {
         this._layers = {}; // Mapping layer id -> layer
+        this._constraints = undefined; // Set in update()
+        this._time = undefined; // Set in setTime()
         _.each(layers, function(l) {this._layers[l.name] = l;}, this);
         this.update(constraints, availableRanges);
     },
@@ -34,11 +36,31 @@ OpenLayers.Layer.Animation.LayerGroupCoordinator = OpenLayers.Class({
         return [begin, end];
     },
 
+    /**
+     * Get currently visible range groups.
+     *
+     * @return {Array<String>} Names of currently visible range
+     * groups. If time is not set or current time is not in any range
+     * group, an empty array is returned.
+     */
+    getCurrentRangeGroups : function() {
+        var result = [];
+        if (this._time !== undefined) {
+            _.each(this._constraints.rangeGroups, function(group, groupId) {
+                if (OpenLayers.Layer.Animation.Utils.inRange(this._time, group.range)) {
+                    result.push(groupId);
+                }
+            }, this);
+        }
+        return result;
+    },
+
         // Constraint object:
         // - globalRange - all availableRanges are limited to this
-        // - rangeGroups {groupId : {range:range, layers:layers}}
+        // - rangeGroups {groupName : {range:range, layers:layers}}
         //   - availableRanges are limited by the first rangeGroup.range whose .layers contains their id
     update : function(constraints, availableRanges) {
+        this._constraints = constraints;
         var constrainedRanges = {};
         _.each(availableRanges, function(range, layerName) {
             var globallyLimitedRange = this.limitRange(range, constraints.globalRange);
@@ -70,9 +92,10 @@ OpenLayers.Layer.Animation.LayerGroupCoordinator = OpenLayers.Class({
 
 
     /**
-     * Set time for all layers
+     * Set time for all layers.
      */
     setTime : function(t) {
+        this._time = t;
         _.invoke(this._layers, "setTime", t);
     }
 
